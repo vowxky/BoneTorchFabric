371724c4c1eaeb8125a646f71a271d83f5d2049e
Vowxky <vowxky@gmail.com>
2025-01-27 19:44:53 -0500

    first commit

---
+++ src/main/java/vowxky/bonetorchfabric/Bonetorchfabric.java
@@ -0,0 +1,54 @@
+package vowxky.bonetorchfabric;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.block.*;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.VerticallyAttachableBlockItem;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Direction;
+
+public class Bonetorchfabric implements ModInitializer {
+    public static final String MOD_ID = "bonetorchfabric";
+
+    public static Block BONE_TORCH;
+    public static Block WALL_BONE_TORCH;
+    public static Item BONE_TORN_ITEM;
+
+    @Override
+    public void onInitialize() {
+        registerBlocks();
+        registerItems();
+        setupRenderLayers();
+    }
+
+    private void registerBlocks() {
+        BONE_TORCH = registerOnlyBlock("bone_torch",
+                new TorchBlock(ParticleTypes.FLAME, AbstractBlock.Settings.copy(Blocks.TORCH).sounds(BlockSoundGroup.BONE)));
+        WALL_BONE_TORCH = registerOnlyBlock("bone_wall_torch",
+                new WallTorchBlock(ParticleTypes.FLAME, AbstractBlock.Settings.copy(Blocks.WALL_TORCH).dropsLike(BONE_TORCH).sounds(BlockSoundGroup.BONE)));
+    }
+
+    private void registerItems() {
+        BONE_TORN_ITEM = registerBlockItem("bone_torch",
+                new VerticallyAttachableBlockItem(BONE_TORCH, WALL_BONE_TORCH, new Item.Settings(), Direction.DOWN));
+    }
+
+    private void setupRenderLayers() {
+        BlockRenderLayerMap.INSTANCE.putBlock(BONE_TORCH, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(WALL_BONE_TORCH, RenderLayer.getCutout());
+    }
+
+    private static Block registerOnlyBlock(String name, Block block) {
+        return Registry.register(Registries.BLOCK, Identifier.of(MOD_ID, name), block);
+    }
+
+    private static Item registerBlockItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(MOD_ID, name), item);
+    }
+}
---
+++ src/main/java/vowxky/bonetorchfabric/client/BonetorchfabricClient.java
@@ -0,0 +1,10 @@
+package vowxky.bonetorchfabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BonetorchfabricClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
